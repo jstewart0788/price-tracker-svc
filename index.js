a7dbb6f351c65b3477121359890160d7c01ce357
jonathan stewart <jstewart0788@gmail.com>
2019-11-23 23:38:50 -0500

    update connection string

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const app = express();
 const api = require('./routes');
 //DB Conenction
 mongoose.connect(
-    `mongodb://${process.env.MONGO_USER_NAME}:${
-      process.env.MONGO_PASSWORD
-    }@ds333248.mlab.com:33248/heroku_3vcm7bjb`,
-    { useNewUrlParser: true }
-  );
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true }
+);
 
 //Middleware
 app.use(logger('dev'));
@@ -26,20 +24,20 @@ app.use('/api', api)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.send(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err);
 });
 
 /**
